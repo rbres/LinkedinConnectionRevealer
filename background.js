5967263679f09082be69976c839568ab6afafe79
Breezy <ryan@ryanbreslow.com>
2015-09-17 00:07:39 -0700

    Updated for new client side rendering and jQuery blocking

--- background.js
+++ background.js
@@ -1,12 +1,28 @@
 if(document.URL.indexOf("linkedin.com") > -1){
-  all_dds = document.getElementsByTagName("dd");
-  table_item = all_dds.item(2);
-  url = "https://www.linkedin.com/" + table_item.firstChild.getAttribute("href");
-  $.ajax(url).done(function(response) { 
-    text = response;
-    start = text.search("followerCount")+15;
-    end = text.search("activityCount")-2;
-    connections = text.substring(start, end);
-    $(".connections-link").text($(".connections-link").text() + " ("+ connections + ")");
-  });
+	var all_dds = document.getElementsByTagName("dd");
+	var table_item = all_dds.item(2);
+	var url = "https://www.linkedin.com" + table_item.firstChild.getAttribute("href");
+	$.ajax(url,{
+	 headers: {
+	   "Upgrade-Insecure-Requests": 1
+	 },
+	 xhr: function() {
+	   var xhr = jQuery.ajaxSettings.xhr();
+	   var setRequestHeader = xhr.setRequestHeader;
+	   xhr.setRequestHeader = function(name, value) {
+	     if (name == 'X-Requested-With') return;
+	     setRequestHeader.call(this, name, value);
+	   }
+	   return xhr;
+	 },
+	}).done(function(response) { 
+	 var text = response;
+	 var start = text.search("ozfeed-templates");
+	 text = text.substr(start);
+	 start = text.search("\"followCount\":") + 14;
+	 text = text.substr(start);
+	 var end = text.search("\"")-1;
+	 text = text.substr(0, end);
+	 $(".connections-link").text($(".connections-link").text() + " ("+ text + ")");
+	});
 }
